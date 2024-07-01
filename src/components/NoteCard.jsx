7dd452cf6381b67af8c3ccafe1bb6164a91aa7f0
Anand . <anandsmathpati18@gmail.com>
2024-07-01 13:36:27 +0530

    [Anand] updated notecard component

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -3,23 +3,28 @@ import { IconButton, Menu, MenuItem } from '@mui/material/';
 import { AddAlertOutlined, PaletteOutlined, PersonAddAltOutlined, MoreVertOutlined, ArchiveOutlined, ImageOutlined, UnarchiveOutlined, Block, Circle, DeleteOutline, RestoreFromTrash } from '@mui/icons-material';
 import { Deleting, updateArchive, updateColor, removeNotes } from '../services/Noteservices';
 
-function NoteCard({ note, action, isTrashNote,getData }) {
-    const [menu, setMenu] = useState(null);
-    const [colorMenu, setColorMenu] = useState(null);
+function NoteCard({ note, action, isTrashNote, getData }) {
+    const [menuAnchorEl, setMenuAnchorEl] = useState(null);
+    const [colorMenuAnchorEl, setColorMenuAnchorEl] = useState(null);
 
-    const open = Boolean(menu);
-    const openColor = Boolean(colorMenu);
-
-    const handleClick = (event) => {
-        setMenu(event.currentTarget);
+    const handleMenuClick = (event) => {
+        setMenuAnchorEl(event.currentTarget);
     };
 
-    const pickColor = (event) => {
-        setColorMenu(event.currentTarget);
+    const handleColorMenuClick = (event) => {
+        setColorMenuAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setMenuAnchorEl(null);
+    };
+
+    const handleColorMenuClose = () => {
+        setColorMenuAnchorEl(null);
     };
 
     const noteColor = async (color) => {
-        setColorMenu(null);
+        handleColorMenuClose();
         note.color = color;
         try {
             await updateColor({ noteIdList: [note.id], color });
@@ -32,61 +37,57 @@ function NoteCard({ note, action, isTrashNote,getData }) {
     const deleteNote = async () => {
         try {
             const deleteData = { noteIdList: [note.id], isDeleted: true };
-            const response = await Deleting(deleteData);
-            console.log('Note deleted:', response);
+            await Deleting(deleteData);
             getData();
         } catch (error) {
             console.error('Error deleting note:', error);
         } finally {
-            setMenu(null);
+            handleMenuClose();
         }
     };
 
     const removeNotePermanently = async () => {
         try {
-            const response = await removeNotes({ noteIdList: [note.id] });
-            console.log('Note permanently deleted:', response);
+            await removeNotes({ noteIdList: [note.id] });
             action();
         } catch (error) {
             console.error('Error permanently deleting note:', error);
         } finally {
-            setMenu(null);
+            handleMenuClose();
         }
     };
 
     const restoreNote = async () => {
         try {
             const restoreData = { noteIdList: [note.id], isDeleted: false };
-            const response = await Deleting(restoreData);
-            console.log('Note restored:', response);
+            await Deleting(restoreData);
             action();
         } catch (error) {
             console.error('Error restoring note:', error);
         } finally {
-            setMenu(null);
+            handleMenuClose();
         }
     };
 
     const toggleArchive = async () => {
         try {
             const archiveData = { noteIdList: [note.id], isArchived: !note.isArchived };
-            const response = await updateArchive(archiveData);
-            console.log('Archive status updated:', response);
+            await updateArchive(archiveData);
             getData();
         } catch (error) {
             console.error('Error updating archive status:', error);
         } finally {
-            setMenu(null);
+            handleMenuClose();
         }
     };
 
     return (
         <div className="flex flex-col justify-between mt-[-40px] h-[110px] min-h[103px] max-h-[385.2px] w-[250px] border-gray-200 rounded-lg border-2 relative m-[10px] group" style={{ backgroundColor: note.color }}>
-            <div className='p-[10px]'>
-                <input value={note.title} type='text' className="text-base font-medium leading-6 pt-3 mb-[4px] outline-none bg-transparent" placeholder='Title' readOnly />
+            <div className='p-[8px]'>
+                <input value={note.title} type='text' className="text-base font-medium leading-6 pt-2 mb-[4px] outline-none bg-transparent" placeholder='Title' readOnly />
                 <textarea value={note.description} className="h-full w-full font-normal leading-5 resize-none outline-none bg-transparent overflow-hidden" readOnly />
             </div>
-            <div className="hidden justify-around mt-[-30px] group-hover:flex">
+            <div className="opacity-0 group-hover:opacity-100 visibility-hidden group-hover:visibility-visible transition-opacity duration-200 ease-in-out flex justify-around mt-[-30px]">
                 {isTrashNote ? (
                     <>
                         <IconButton onClick={restoreNote} title='Restore' className='!w-[35px] !min-w-0' color="inherit"><RestoreFromTrash style={{ fontSize: 18 }} /></IconButton>
@@ -96,15 +97,15 @@ function NoteCard({ note, action, isTrashNote,getData }) {
                     <>
                         <IconButton title='Remind me' className='!w-[35px] !min-w-0' color="inherit"><AddAlertOutlined style={{ fontSize: 18 }} /></IconButton>
                         <IconButton title='Collaborator' className='!w-[35px] !min-w-0' color="inherit"><PersonAddAltOutlined style={{ fontSize: 18 }} /></IconButton>
-                        <IconButton onClick={pickColor} title='Background options' className='!w-[35px] !min-w-0' color="inherit"><PaletteOutlined style={{ fontSize: 18 }} /></IconButton>
+                        <IconButton onClick={handleColorMenuClick} title='Background options' className='!w-[35px] !min-w-0' color="inherit"><PaletteOutlined style={{ fontSize: 18 }} /></IconButton>
                         <IconButton title='Add image' className='!w-[35px] !min-w-0' color="inherit"><ImageOutlined style={{ fontSize: 16 }} /></IconButton>
                         {note.isArchived ? (
                             <IconButton onClick={toggleArchive} title='Unarchive' className='!w-[35px] !min-w-0' color="inherit"><UnarchiveOutlined style={{ fontSize: 18 }} /></IconButton>
                         ) : (
                             <IconButton onClick={toggleArchive} title='Archive' className='!w-[35px] !min-w-0' color="inherit"><ArchiveOutlined style={{ fontSize: 18 }} /></IconButton>
                         )}
-                        <IconButton onClick={handleClick} title='More' className='!w-[35px] !min-w-0' aria-controls="simple-menu" aria-haspopup="true" color="inherit"><MoreVertOutlined /></IconButton>
-                        <Menu id="simple-menu" open={open} onClose={() => setMenu(null)} anchorEl={menu}>
+                        <IconButton onClick={handleMenuClick} title='More' className='!w-[35px] !min-w-0' aria-controls="simple-menu" aria-haspopup="true" color="inherit"><MoreVertOutlined /></IconButton>
+                        <Menu id="simple-menu" open={Boolean(menuAnchorEl)} onClose={handleMenuClose} anchorEl={menuAnchorEl}>
                             <MenuItem onClick={deleteNote}>Delete</MenuItem>
                             <MenuItem>Share</MenuItem>
                             <MenuItem>Add Label</MenuItem>
@@ -112,7 +113,7 @@ function NoteCard({ note, action, isTrashNote,getData }) {
                     </>
                 )}
             </div>
-            <Menu open={openColor} onClose={() => setColorMenu(null)} anchorEl={colorMenu}>
+            <Menu open={Boolean(colorMenuAnchorEl)} onClose={handleColorMenuClose} anchorEl={colorMenuAnchorEl}>
                 <div className='flex'>
                     <IconButton onClick={() => noteColor('#FFFFFF')}><Block /></IconButton>
                     <IconButton onClick={() => noteColor('#94bbe9')}><Circle sx={{ color: '#94bbe9' }} /></IconButton>
@@ -128,4 +129,6 @@ function NoteCard({ note, action, isTrashNote,getData }) {
 }
 
 export default NoteCard;
+
+
 
